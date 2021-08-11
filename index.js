a4ed06dbcc0b97c3ac060b284586e2d70036f1da
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-12 05:06:53 +0800

    feat: post request for new sighting.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { add } from './jsonFileStorage.js';
 
 const app = express();
 const PORT = process.argv[2];
@@ -6,9 +7,35 @@ const FILENAME = './data.json';
 
 // Set view engine
 app.set('view engine', 'ejs');
+// To receive POST request body data in request.body
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/sighting', (request, response) => {
   response.render('sighting');
+});
+
+app.post('/sighting', (request, response) => {
+  const textLength = request.body.text.length;
+  const sighting = {
+    ...request.body,
+    summary: request.body.text
+      .substring(0, 150)
+      .concat(
+        (textLength > 150) ? '...' : '',
+      ),
+  };
+  // Add new recipe data in request.body to recipes array in data.json.
+  add(FILENAME, 'sightings', sighting, (err, str) => {
+    if (err) {
+      response.status(500).send('DB write error.');
+      return;
+    }
+
+    console.log('Successful creation of new sighting.');
+    console.log('Sighting:', sighting);
+
+    response.status(200).send('Submitted successfully!');
+  });
 });
 
 app.listen(PORT);
