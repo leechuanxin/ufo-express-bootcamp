01235bc01d027621aa9005e4f08e341069d57063
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-12 04:43:52 +0800

    feat: report a new sighting page.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+import express from 'express';
+
+const app = express();
+const PORT = process.argv[2];
+const FILENAME = './data.json';
+
+// Set view engine
+app.set('view engine', 'ejs');
+
+app.get('/sighting', (request, response) => {
+  response.render('sighting');
+});
+
+app.listen(PORT);
