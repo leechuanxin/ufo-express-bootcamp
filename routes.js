21986a10dd52e7c80f55741e698d2bc0a06169f9
Lee Chuan Xin <chuanxin@gametize.com>
2021-08-15 19:19:49 +0800

    feat: sighting data validation.

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
 import moment from 'moment';
 import { add, read, write } from './jsonFileStorage.js';
-import standardizeParam from './util.js';
+import { standardizeParam, validateSighting } from './util.js';
 
 const FILENAME = './data.json';
 const SUMMARY_CHAR_LIMIT = 100;
+// 6 keys for city, state, date/time, shape, duration, story
+const SIGHTING_EDIT_KEY_LENGTH = 6;
 
 export const handleIndex = (request, response) => {
   read(FILENAME, (err, data) => {
@@ -123,82 +125,91 @@ export const handleSightingEdit = (request, response) => {
 
 export const handleSightingEditPut = (request, response) => {
   const idxParam = request.params.index;
+  const validatedSighting = validateSighting(request.body, response);
   if (Number.isNaN(Number(idxParam))) {
     response.status(406).send('Your sighting ID has to be a number!');
-  }
-  read(FILENAME, (err, data) => {
-    // page indexes/ids start from 1 instead of 0
-    if (idxParam > data.sightings.length || idxParam < 1) {
-      response.status(404).send('Sorry, we cannot find that!');
-    } else {
-      const textLength = request.body.text.length;
+  } else if (
+    validatedSighting
+    && Object.keys(validatedSighting).length === SIGHTING_EDIT_KEY_LENGTH
+  ) {
+    read(FILENAME, (err, data) => {
       // page indexes/ids start from 1 instead of 0
-      data.sightings[idxParam - 1] = {
-        ...request.body,
-        summary: request.body.text
-          .substring(0, SUMMARY_CHAR_LIMIT)
-          .concat(
-            (textLength > SUMMARY_CHAR_LIMIT) ? '...' : '',
-          ),
-        created: data.sightings[idxParam - 1].created || new Date(),
-        lastUpdated: new Date(),
-      };
-      write(FILENAME, data, (error) => {
-        if (error) {
-          response.status(500).send('DB write error. We cannot edit this sighting. Please try again!');
-        }
-        response.redirect(`/sighting/${idxParam}`);
-      });
-    }
-  });
+      if (idxParam > data.sightings.length || idxParam < 1) {
+        response.status(404).send('Sorry, we cannot find that!');
+      } else {
+        const textLength = validatedSighting.text.length;
+        // page indexes/ids start from 1 instead of 0
+        data.sightings[idxParam - 1] = {
+          ...validatedSighting,
+          summary: validatedSighting.text
+            .substring(0, SUMMARY_CHAR_LIMIT)
+            .concat(
+              (textLength > SUMMARY_CHAR_LIMIT) ? '...' : '',
+            ),
+          created: data.sightings[idxParam - 1].created || new Date(),
+          lastUpdated: new Date(),
+        };
+        write(FILENAME, data, (error) => {
+          if (error) {
+            response.status(500).send('DB write error. We cannot edit this sighting. Please try again!');
+          }
+          response.redirect(`/sighting/${idxParam}`);
+        });
+      }
+    });
+  }
 };
 
 export const handleSightingDelete = (request, response) => {
   const idxParam = request.params.index;
   if (Number.isNaN(Number(idxParam))) {
     response.status(406).send('Your sighting ID has to be a number!');
-  }
-  // Remove element from DB at given index
-  read(FILENAME, (err, data) => {
-    // page indexes/ids start from 1 instead of 0
-    if (request.params.index > data.sightings.length || request.params.index < 1) {
-      response.status(404).send('Sorry, we cannot find that!');
-    } else {
+  } else {
+    // Remove element from DB at given index
+    read(FILENAME, (err, data) => {
       // page indexes/ids start from 1 instead of 0
-      data.sightings.splice(idxParam - 1, 1);
-      write(FILENAME, data, (error) => {
-        if (!error) {
-          response.redirect('/');
-        } else {
-          response.status(500).send('DB write error. We cannot delete this sighting. Please try again!');
-        }
-      });
-    }
-  });
+      if (request.params.index > data.sightings.length || request.params.index < 1) {
+        response.status(404).send('Sorry, we cannot find that!');
+      } else {
+        // page indexes/ids start from 1 instead of 0
+        data.sightings.splice(idxParam - 1, 1);
+        write(FILENAME, data, (error) => {
+          if (!error) {
+            response.redirect('/');
+          } else {
+            response.status(500).send('DB write error. We cannot delete this sighting. Please try again!');
+          }
+        });
+      }
+    });
+  }
 };
 
 export const handleSightingCreate = (request, response) => {
   const textLength = request.body.text.length;
-  const sighting = {
-    ...request.body,
-    summary: request.body.text
-      .substring(0, SUMMARY_CHAR_LIMIT)
-      .concat(
-        (textLength > SUMMARY_CHAR_LIMIT) ? '...' : '',
-      ),
-    created: new Date(),
-    lastUpdated: new Date(),
-  };
-  // Add new recipe data in request.body to recipes array in data.json.
-  add(FILENAME, 'sightings', sighting, (err, str) => {
-    if (err) {
-      response.status(500).send('DB write error.');
-      return;
-    }
+  const validatedSighting = validateSighting(request.body, response);
+  if (validatedSighting && Object.keys(validatedSighting).length === SIGHTING_EDIT_KEY_LENGTH) {
+    const sighting = {
+      ...validatedSighting,
+      summary: validatedSighting.text
+        .substring(0, SUMMARY_CHAR_LIMIT)
+        .concat(
+          (textLength > SUMMARY_CHAR_LIMIT) ? '...' : '',
+        ),
+      created: new Date(),
+      lastUpdated: new Date(),
+    };
+    // Add new recipe data in request.body to recipes array in data.json.
+    add(FILENAME, 'sightings', sighting, (err, str) => {
+      if (err) {
+        response.status(500).send('DB write error.');
+        return;
+      }
 
-    const obj = JSON.parse(str);
-    // page indexes/ids start from 1 instead of 0
-    const idx = obj.sightings.length;
-    response.redirect(`/sighting/${idx}`);
-  });
+      const obj = JSON.parse(str);
+      // page indexes/ids start from 1 instead of 0
+      const idx = obj.sightings.length;
+      response.redirect(`/sighting/${idx}`);
+    });
+  }
 };
--- util.js
+++ util.js
@@ -1,5 +1,7 @@
+import moment from 'moment';
+
 // replace all spaces, dashes and underscores
-const standardizeParam = (str) => str
+export const standardizeParam = (str) => str
   .trim()
   .toLowerCase()
   .split('%20')
@@ -11,4 +13,42 @@ const standardizeParam = (str) => str
   .split('-')
   .join('');
 
-export default standardizeParam;
+export const validateSighting = (sighting, res) => {
+  const currentTime = new Date();
+  const dateTimeValidated = new Date(sighting.date_time);
+  if (!sighting.city || sighting.city.trim() === '') {
+    res.status(400).send('Please enter a valid city of your UFO sighting!');
+    return false;
+  }
+
+  if (!sighting.state || sighting.state.trim === '') {
+    res.status(400).send('Please enter a valid state of your UFO sighting!');
+    return false;
+  }
+
+  if (
+    !(dateTimeValidated instanceof Date)
+    || Number.isNaN(Number(dateTimeValidated.valueOf()))
+    || dateTimeValidated > currentTime
+  ) {
+    res.status(400).send(`Please enter a valid date and time before ${moment(currentTime).format('MMMM DD, YYYY hh:mmA')} for your UFO sighting!`);
+    return false;
+  }
+
+  if (!sighting.shape || sighting.shape.trim === '') {
+    res.status(400).send('Please enter a valid shape of your UFO!');
+    return false;
+  }
+
+  if (!sighting.duration || sighting.duration.trim === '') {
+    res.status(400).send('Please enter a valid duration of your UFO sighting!');
+    return false;
+  }
+
+  if (!sighting.text || sighting.text.trim === '') {
+    res.status(400).send('Please tell us more about your UFO sighting!');
+    return false;
+  }
+
+  return sighting;
+};
